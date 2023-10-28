5acd23803b884216a6eebd29e81debd8260cf22d
Kasra Rismanchi <kasra.rismanchi@instructure.com>
2023-10-29 00:44:00 +0200

    init plugin

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { ViteDevServer } from "vite";
+import chokidar from "chokidar";
+
+export default function dependencyWatcher(
+  dependenciesPath: string[] = [],
+  dependenciesName: string[] = []
+) {
+  return {
+    name: "dependency-watcher",
+    //watching dependencies for file changes and reloads the browser
+    configureServer(server: ViteDevServer) {
+      const watcher = chokidar.watch(dependenciesPath);
+      watcher.on("change", () => {
+        server.ws.send({ type: "full-reload" });
+      });
+
+      server.middlewares.use((req, res, next) => {
+        if (dependenciesName.some((name) => req.url?.includes(name))) {
+          if (req.url) {
+            req.url += Date.now();
+            res.setHeader("Expires", "0");
+            res.setHeader("Pragma", "no-cache");
+          }
+        }
+        next();
+      });
+    },
+  };
+}
